83b429190f4c425b8dd5d8da117fa398612cd3ef
Parsa Mallik <pmallik22@bowie.cs.earlham.edu>
2023-02-28 17:11:00 -0500

    first attempt at classroom class

--- room-occupancy-base.py
+++ room-occupancy-base.py
@@ -1,2 +1,13 @@
+## making the classroom as the base class?
+
 class Classroom:
-    
+    def __init__(self, empty, occupied):
+        self.empty = empty
+        self.occupied = occupied
+
+    def availability(self):
+        ## use an if statement using the input from the google form
+        if yes:   ## assuming the question in the google form was "Are you using the room now?" with yes or no options
+            return self.occupied
+        else:
+            return self.empty

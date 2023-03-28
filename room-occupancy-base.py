e7d3c989367aa21771430233cb70cb53dc1ba9d0
Parsa Mallik <pmallik22@bowie.cs.earlham.edu>
2023-03-28 18:08:19 -0400

    w/ Tien

--- room-occupancy-base.py
+++ room-occupancy-base.py
@@ -2,30 +2,43 @@
 
 class Classroom:
     def __init__(self, empty, roomNum):
-        self.roomNum = roomNum
-        self.empty = empty
+        self.roomNum = roomNum  #example: cst_101
+        self.empty = green
+        self.occupied = red
 
     def availability(self):
         ## use an if statement using the input from the google form
-        if yes:   ## assuming the question in the google form was "Are you using the room now?" with yes or no options
-            return self.occupied
-        else:
-            return self.empty
+        # Options on QR code page: "I am entering the room" and "I am leaving the room"
+        if entering:   #entering
+            return self.occupied  # return red
+        else:  #leaving
+            return self.empty   # return green
 
-Buildings = [CST, LBC]
-CST = [100, 101, ...]
-100 = Classroom()
-buildings = {
-    cst:[Classroom("101"),],
-    dennis:[],
-    lbc:[],
-    carpenter:[],
-    library:[],
-    others:[],
-    runyan:[],
-    runyan:[],
-    }
+    def create_room(self, building, roomNum):   # building: name of list (key in dict), roomNum: building_num
 
+# make a class called building so that making change is easier
+
+if __name__ == "__main__": 
+    buildings = {
+        cst:[Classroom("101"),],
+        dennis:[],
+        lbc:[],
+        carpenter:[],
+        library:[],
+        others:[],
+        runyan:[],
+        runyan:[],
+        }
+    # make a file where we store all the rooms/ buildings
+    # make a function that can add/ delete rooms in that file
+
+    # make a generalized code 
+
+    cst = building[cst]  # calling the key cst and storing the list(value) as cst
+    color = cst[0].availability()
+    # in the map
+    cst_101 = color  # going to color the room on the map as red or green
 
 # make a building class with list of classroom classes
 # saving the data because of power failure and loss of data, write the data to a file and read from there
+# we have two parts: the display on website, data under the display
